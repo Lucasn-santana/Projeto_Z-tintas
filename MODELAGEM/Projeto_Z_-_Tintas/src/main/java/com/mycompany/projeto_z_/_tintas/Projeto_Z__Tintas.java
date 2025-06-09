6ab5d44682b22610afeb7dd2985411ada8c6f320
Matheus Targa <matheus.targa@gmail.com>
2025-06-09 19:37:03 -0300

    Inicio do projeto funcional

---
+++ MODELAGEM/Projeto_Z_-_Tintas/src/main/java/com/mycompany/projeto_z_/_tintas/Projeto_Z__Tintas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_z_._tintas;
+
+/**
+ *
+ * @author m.targa
+ */
+public class Projeto_Z__Tintas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
