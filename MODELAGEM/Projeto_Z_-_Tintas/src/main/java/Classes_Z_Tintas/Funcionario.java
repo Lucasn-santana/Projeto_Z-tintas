935bc687de1f13a04f32329a2ba76d5ccfdad29c
Matheus Targa <matheus.targa@gmail.com>
2025-06-10 21:40:01 -0300

    Criando o esboço das Classes

---
+++ MODELAGEM/Projeto_Z_-_Tintas/src/main/java/Classes_Z_Tintas/Funcionario.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes_Z_Tintas;
+
+/**
+ *
+ * @author m.targa
+ */
+public class Funcionario {
+    /*Variaveis/Dados necessarios
+        -id_funcionario INT PK
+        -nome VARCAHR(45)
+        -cargo VARCHAR(45)
+        -Login_id_login INT FK
+    */
+}
