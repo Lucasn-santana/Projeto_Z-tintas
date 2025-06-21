f5684720bffd74db205100434dd8554cdbb66db4
matheus targa <matheus.targa@gmail.com>
2025-06-21 18:11:21 -0300

    Criando as classes funcionario e cliente

---
+++ DESENVOLVIMENTO/Z_TINTAS/src/main/java/com/mycompany/z_tintas/Classe_Funcionario.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.z_tintas;
+
+/**
+ *
+ * @author Matheus
+ */
+public class Classe_Funcionario {
+ 
+
+    private int idFuncionario;
+    private String nomeFuncionario;
+    private String cargoFuncionario;
+    private String senha;
+
+    public int getIdFuncionario() {
+        return idFuncionario;
+    }
+
+    public void setIdFuncionario(int idFuncionario) {
+        this.idFuncionario = idFuncionario;
+    }
+
+    public String getNomeFuncionario() {
+        return nomeFuncionario;
+    }
+
+    public void setNomeFuncionario(String nomeFuncionario) {
+        this.nomeFuncionario = nomeFuncionario;
+    }
+
+    public String getCargoFuncionario() {
+        return cargoFuncionario;
+    }
+
+    public void setCargoFuncionario(String cargoFuncionario) {
+        this.cargoFuncionario = cargoFuncionario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    public void Cadastrar_Funcionario(String nomeFuncionario,String cargoFuncionario,String senha){
+        this.nomeFuncionario = nomeFuncionario;
+        this.cargoFuncionario = cargoFuncionario;
+        this.senha = senha;
+        
+    }
+
+}
+   
+
